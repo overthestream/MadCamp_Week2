384b12a029ca68b37af46e7e354e183c95eaf8e6
overthestream <njhnjh02@naver.com>
2022-01-09 16:34:30 +0900

    kakaaaaaaaao

--- src/navigation/InitialScreen.js
+++ src/navigation/InitialScreen.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, StatusBar, Image } from 'react-native';
 import { NavigationService } from '../common';
+import loginButtonImage from '../img/kakao_login.png'
+import { Linking } from 'expo';
+import { login } from '@react-native-seoul/kakao-login';
 
 const InitialScreen = ({navigation, route}) => {
     return(
         <View style={styles.container}>
                 <StatusBar barStyle="dark-content" />
                 <TouchableOpacity
-                        onPress={()=> navigation.navigate('HomeScreen')}
+                        onPress={
+                            
+                            loginWithKaKao
+                        }
                         style={{
                             justifyContent: 'flex-end',
-                            backgroundColor: '#FF9776',
-                            padding: 20,
-                            marginTop: 20,
-                            marginLeft: 50,
-                            borderRadius: 30,
-                            width: '70%',
+                            marginLeft:'10%',
+                            marginRight:'10%'
                         }}>
-                        <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>로그인</Text>
+                        <Image 
+                            source ={loginButtonImage}
+                            style ={{                           
+                                resizeMode:'stretch',
+                                width: '100%'
+                            }}/>
                     </TouchableOpacity>
-                <TouchableOpacity
-                        onPress={()=> navigation.navigate('SetMbtiScreen')}
-                        style={{
-                            justifyContent: 'flex-end',
-                            backgroundColor: '#FF9776',
-                            padding: 20,
-                            marginTop: 20,
-                            marginLeft: 50,
-                            borderRadius: 30,
-                            width: '70%',
-                        }}>
-                        <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>회원가입</Text>
-                </TouchableOpacity>
             </View>
     );
+}
+
+const loginWithKaKao = async () => {
+    try{
+        //navigation.navigate('HomeScreen')}
+        const token =await login();
+        console.log(token);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 const styles = StyleSheet.create({
