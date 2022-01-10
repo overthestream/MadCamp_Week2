12807ba1f9d39edfa527f1e1913dcd0c12ab7548
yunz0926 <chldbswl9@gmail.com>
2022-01-11 00:25:39 +0900

    Fix: fix reducer bug

--- src/reducers/friend.js
+++ src/reducers/friend.js
@@ -1,6 +1,8 @@
 const ADD_FRIEND = 'FRIEND/ADD';
+const DELETE_FRIEND = 'FRIEND/DELETE'
 
 export const addFriend = (id) => ({type: ADD_FRIEND, payload: id});
+export const deleteFriend = (id) => ({type: DELETE_FRIEND, payload: id});
 
 const initialState = {
     friends: [],
@@ -11,8 +13,11 @@ export default friend = (state = initialState, action) => {
         case ADD_FRIEND:
             const id = action.payload;
             return {
-                ...state,
-                friends: [...state.friends, id], 
+                friends: state.friends.concat(action.payload)
+            };
+        case DELETE_FRIEND:
+            return {
+                friends: state.friends.filter((i) => i!==action.payload),
             };
         default:
             return {...state};
