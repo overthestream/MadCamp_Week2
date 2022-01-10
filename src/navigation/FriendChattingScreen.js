2958429c38997605739528c21624604e4acdcca9
overthestream <njhnjh02@naver.com>
2022-01-11 05:34:23 +0900

    와넛ㅇ~

--- src/navigation/FriendChattingScreen.js
+++ src/navigation/FriendChattingScreen.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { GiftedChat, InputToolbar, Bubble, Actions, Send } from 'react-native-gifted-chat';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {io} from 'socket.io-client'
+import axios from 'axios';
 
 //웹 소켓으로부터 받아온 Message를 넣으면 됨
 
@@ -36,67 +38,73 @@ const renderbubble = props => {
   );
 }
 
-const FriendChattingScreen = ({ navigation }) => {
+const FriendChattingScreen = ({ navigation, route }) => {
     const [text, setText] = useState("");
     const [messages, setMessages] = useState([]);
     const state = useSelector((state) => state);
     const { id, nickname } = state.user;
-    
+    const [incre, setIncre] = useState(1)
     const dispatch = useDispatch();
+    const {friendId} = route.params;
 
-    const addfriend = () => {
-      dispatch(addFriend(2));
-    };
 
+
+    
     const user = {
         _id: id,
         name: nickname,
       };
-    /* 메시지 수신 부분
+    let socket;
     useEffect(() => {
-    socket.emit('loadMessages', {});
-    socket.on('loadMessages', msg => {
-      setMessages(msg);
-    })
-  }, []);
-    */
-      
 
-    useEffect(() => {
-        setMessages([
-          {
-            _id: 1,
-            text: 'Hello developer',
-            createdAt: new Date(),
+      const fetchData = async () => {
+        try {
+        const result = await axios.get(`http://192.249.18.173/friend/talk?from=${id}&to=${friendId}`);
+        const list = result.data.map((item) => {
+          const obj = {
+            _id:incre,
+            text: item.talk_message,
+            createdAt: item.send_at,
+            user: id == item.send_from ? user : {
+              _id:2,
+              name: item.send_from,
+
+              avatar: require(`../img/INTP.png`),
+            }
+          }
+          setIncre(incre+1);
+          return obj;
+        })
+        console.log('hiiiih');
+        setMessages(list);}
+        catch (err) {
+          console.log(err);
+        }
+      }
+      fetchData();
+      socket = io('http://192.249.18.173:80');
+      socket.on('connect', (io) => {
+          console.log('connected');
+      })
+      socket.on('receiveMsg', msg => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages,{
+            _id: incre,
+            text: msg.message,
+            createdAt: msg.timestamp,
             user: {
               _id: 2,
-              name: 'React Native',
-              avatar: require('../img/INTP.png'),
-            },
-          },
-          {
-              _id: 2,
-              text: 'Hello Avatar',
-              createdAt: new Date(),
-              user,
-          },
-          {
-            _id: 3,
-            text: 'This is Test',
-            createdAt: new Date(),
-            user,
-          },
-          {
-            _id: 4,
-            text: 'This is Test2',
-            createdAt: new Date(),
-            user,
-        },
-        ])
-      }, [])
+              name: msg.senderID,
+              avatar: require(`../img/INTP.png`),
+            }}));
+        setIncre(incre+1);
+      })
+    }, []);
+
 
       //메시지 송신 부분
       const onSend = useCallback((messages = []) => {
+        setIncre(incre+1);
+        socket.emit('sendTalk',{message: messages[0].text, senderSocket: socket.id, senderID:id, receiverID: friendId, timestamp: new Date()})
         setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
       }, [])
    
