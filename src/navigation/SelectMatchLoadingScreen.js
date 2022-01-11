19b635f8b29022785cb87c489952cc0b5b51fe27
yunz0926 <chldbswl9@gmail.com>
2022-01-11 16:21:53 +0900

    Feat: Alert EmptySelect

--- src/navigation/SelectMatchLoadingScreen.js
+++ src/navigation/SelectMatchLoadingScreen.js
@@ -42,43 +42,44 @@ const SelectMatchLoadingScreen = ({ route, navigation }) => {
     }, []);
 
     const selectAlert = () => {
+        console.log('empty select');
         alert('한 개 이상의 MBTI를 선택해주세요');
         navigation.goBack();
     }
 
-    if (select === []){
-        console.log('null select');
-        return {selectAlert};
-    }
-    else{
-        return(
-            <View style={{
-                marginTop: '50%',
-                alignItems:'center'
-            }
-            }>
-                <ActivityIndicator size="large" color="#56A7A7"/>
-                <Text>매칭 중..</Text>
-                <TouchableOpacity
-                    onPress={() => { 
-                        //매칭 취소
-                        navigation.goBack();
-                    }}
-                    style={{
-                        justifyContent: 'flex-end',
-                        backgroundColor: '#56A7A7',
-                        padding: 20,
-                        marginTop: '50%',
-                        marginBottom: 20,
-                        borderRadius: 30,
-                        width: 150,
-                        height: 60,
-                }}>
-                    <Text style={{fontFamily: 'aTitleGothic', fontSize: 17, textAlign: 'center', color: 'white'}}>매칭 취소</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    };
+
+    return(
+        <>
+        {
+            select===[] ? {selectAlert} : (
+         <View style={{
+           marginTop: '50%',
+            alignItems:'center'
+        }}>
+            <ActivityIndicator size="large" color="#56A7A7"/>
+            <Text>매칭 중..</Text>
+            <TouchableOpacity
+                onPress={() => { 
+                    //매칭 취소
+                    navigation.goBack();
+                }}
+                style={{
+                    justifyContent: 'flex-end',
+                    backgroundColor: '#56A7A7',
+                    padding: 20,
+                    marginTop: '50%',
+                    marginBottom: 20,
+                    borderRadius: 30,
+                    width: 150,
+                    height: 60,
+            }}>
+                <Text style={{fontFamily: 'aTitleGothic', fontSize: 17, textAlign: 'center', color: 'white'}}>매칭 취소</Text>
+            </TouchableOpacity>
+        </View>
+            )
+        }
+        </>
+    )
 }
 
 
